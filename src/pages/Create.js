f5c1b533723d3358f4609e114d8c0ae02e6f4c7f
Akash1012 <gupta.akash2090@gmail.com>
2021-05-11 12:53:16 +0530

    Initial Setup

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,22 @@
 import React from 'react'
 
+import { Typography, Button, Container } from '@material-ui/core'
+
 export default function Create() {
-  return (
-    <div>
-      Create page
-    </div>
-  )
+    return (
+        <Container>
+            <Typography
+                variant="h6"
+                component="h2"
+                gutterBottom
+                color="primary"
+            >
+                Create a New Note
+            </Typography>
+            <Button onClick={() => console.log("You click mee")}
+                type="submit" color="secondary" variant="contained">
+                Submit
+            </Button>
+        </Container>
+    )
 }
